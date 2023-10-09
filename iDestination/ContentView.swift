00002b25f71f069c05ed07e4f0f8fb75e812a75c
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2023-10-09 17:33:56 -0400

    Initial Commit

---
+++ iDestination/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iDestination
+//
+//  Created by Parth Antala on 2023-10-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iDestination/iDestinationApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDestinationApp.swift
+//  iDestination
+//
+//  Created by Parth Antala on 2023-10-09.
+//
+
+import SwiftUI
+
+@main
+struct iDestinationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
